492db0fc9eba2a03719f137becaa5ced298f97ec
Hang <64747715+xx529@users.noreply.github.com>
2024-08-15 15:55:34 +0800

    data

--- app/run_creator.py
+++ app/run_creator.py
@@ -34,65 +34,71 @@ logger.info(f'共{len(df_group)}个抖音号需要收集')
 try:
     for idx, (streamer_id, titles, publish_datetime_ls) in df_group.iterrows():
         logger.info(f'开始第 {idx + 1} 个，抖音号：{streamer_id}')
-        chrome.click('达人列表')
-        chrome.click('达人搜索（激活前）')
-        chrome.send('达人搜索（激活后）', streamer_id)
-        chrome.wait(2)
-        if chrome.get_element('搜索弹出框', timeout=5).text == '暂无数据':
-            logger.warning(f'未找到抖音号：{streamer_id}')
-            df.loc[df['抖音号'] == streamer_id, '备注'] = '未找到抖音号'
-            df.loc[df['抖音号'] == streamer_id, '抖音播放量'] = 0
-            continue
+        try:
+            chrome.click('达人列表')
+            chrome.click('达人搜索（激活前）')
+            chrome.send('达人搜索（激活后）', streamer_id)
+            chrome.wait(2)
+            if chrome.get_element('搜索弹出框', timeout=5).text == '暂无数据':
+                logger.warning(f'未找到抖音号：{streamer_id}')
+                df.loc[df['抖音号'] == streamer_id, '备注'] = '未找到抖音号'
+                df.loc[df['抖音号'] == streamer_id, '抖音播放量'] = 0
+                continue
 
-        chrome.click('搜索弹出框')
-        chrome.wait(1)
-        chrome.click('主播详情')
-        chrome.click('筛选时间')
+            chrome.click('搜索弹出框')
+            chrome.wait(1)
+            chrome.click('主播详情')
+            chrome.click('筛选时间')
 
-        max_delta_day = get_max_delta_days(publish_datetime_ls)
-        logger.info(f'最大天数：{max_delta_day}')
-        if max_delta_day <= 7:
-            chrome.click('7天')
-        elif max_delta_day <= 14:
-            chrome.click('14天')
-        elif max_delta_day <= 30:
-            chrome.click('30天')
-        else:
-            chrome.click('90天')
-
-        chrome.wait(2)
-
-        if (total := int(chrome.get_element('视频统计').text.split()[1])) == 0:
-            logger.warning(f'抖音号({streamer_id})未找到视频')
-            df.loc[df['抖音号'] == streamer_id, '备注'] = '未找到视频'
-            df.loc[['抖音号'] == streamer_id, '抖音播放量'] = 0
-            continue
-
-        logger.info(f'视频总数：{total}')
-        while True:
-            details = chrome.get_elements('视频明细')
-            logger.info(f'当前视频数：{len(details)}')
-            if len(details) >= total:
-                break
+            max_delta_day = get_max_delta_days(publish_datetime_ls)
+            logger.info(f'最大天数：{max_delta_day}')
+            if max_delta_day <= 7:
+                chrome.click('7天')
+            elif max_delta_day <= 14:
+                chrome.click('14天')
+            elif max_delta_day <= 30:
+                chrome.click('30天')
             else:
-                chrome.scroll_to_button()
-                chrome.wait(1)
+                chrome.click('90天')
 
-        title2count = {}
+            chrome.wait(2)
 
-        for detail in details:
-            title = chrome.get_sub_elements(detail, '视频标题')[0].text
-            count = chrome.get_sub_elements(detail, '统计元素')[0].text
-            title2count[title] = normalize_number(count)
+            if (total := int(chrome.get_element('视频统计').text.split()[1])) == 0:
+                logger.warning(f'抖音号({streamer_id})未找到视频')
+                df.loc[df['抖音号'] == streamer_id, '备注'] = '未找到视频'
+                df.loc[['抖音号'] == streamer_id, '抖音播放量'] = 0
+                continue
 
-        for title in titles:
-            index = ((df['视频标题'] == title) & (df['抖音号'] == streamer_id))
-            if title in title2count:
-                df.loc[index, '抖音播放量'] = int(title2count[title])
-                df.loc[index, '备注'] = '完成'
-            else:
-                df.loc[index, '抖音播放量'] = 0
-                df.loc[index, '备注'] = '未找到视频'
+            logger.info(f'视频总数：{total}')
+            while True:
+                details = chrome.get_elements('视频明细')
+                logger.info(f'当前视频数：{len(details)}')
+                if len(details) >= total:
+                    break
+                else:
+                    chrome.scroll_to_button()
+                    chrome.wait(1)
+
+            title2count = {}
+
+            for detail in details:
+                title = chrome.get_sub_elements(detail, '视频标题')[0].text
+                count = chrome.get_sub_elements(detail, '统计元素')[0].text
+                title2count[title] = normalize_number(count)
+
+            for title in titles:
+                index = ((df['视频标题'] == title) & (df['抖音号'] == streamer_id))
+                if title in title2count:
+                    df.loc[index, '抖音播放量'] = int(title2count[title])
+                    df.loc[index, '备注'] = '完成'
+                else:
+                    df.loc[index, '抖音播放量'] = 0
+                    df.loc[index, '备注'] = '未找到视频'
+
+        except Exception as e:
+            logger.error(e)
+            logger.error(f'抖音号({streamer_id})异常')
+
 except Exception as e:
     logger.error(e)
 
