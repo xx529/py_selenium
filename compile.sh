a4f68f994c2fd9d75761c7f3aec13fef4bc0f578
Hang <64747715+xx529@users.noreply.github.com>
2024-08-21 20:57:36 +0800

    data

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-pyinstaller --onefile --distpath dist your_script.py
+rm -rf dist/*
+rm -rf temp/*
+
+pyinstaller --onefile --name 主播统计.exe --distpath dist --workpath temp --specpath temp app/run_streamer.py
+pyinstaller --onefile --name 创作者中心.exe --distpath dist --workpath temp --specpath temp app/run_creator.py
+pyinstaller --onefile --name 服务平台.exe --distpath dist --workpath temp --specpath temp app/run_platform.py
