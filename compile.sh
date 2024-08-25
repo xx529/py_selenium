9b42787b010706afe7a8da89a293b717a0e18d3a
Hang <64747715+xx529@users.noreply.github.com>
2024-08-25 22:20:52 +0800

    data

--- compile.sh
+++
@@ -1,6 +0,0 @@
-rm -rf dist/*
-rm -rf temp/*
-
-pyinstaller --onefile --name 主播统计.exe --distpath dist --workpath temp --specpath temp app/run_streamer.py
-pyinstaller --onefile --name 创作者中心.exe --distpath dist --workpath temp --specpath temp app/run_creator.py
-pyinstaller --onefile --name 服务平台.exe --distpath dist --workpath temp --specpath temp app/run_platform.py
